ba464e6833fa2e2c9cfda45b90deb453460cbd5b
khr-rakib <khr.rakib@gmail.com>
2020-08-16 19:33:53 +0600

    Updates

---
+++ precache-manifest.5416d1e5eac60d3e1eaa3fa5516bdda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584e7d16efe50bb9a5af1a7c0ac79432",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "20e94370df224764956a",
+    "url": "/react-pagination/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "eeb76fc7e63f7d3955f7",
+    "url": "/react-pagination/static/js/2.fd3c8fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.fd3c8fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e94370df224764956a",
+    "url": "/react-pagination/static/js/main.d3efa18a.chunk.js"
+  },
+  {
+    "revision": "748902ce97adf4002e0b",
+    "url": "/react-pagination/static/js/runtime-main.88ad8ab5.js"
+  }
+]);
