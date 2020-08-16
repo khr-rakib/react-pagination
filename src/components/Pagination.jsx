123c205a58cc6b446eeb2597223bd9590f68612d
khr-rakib <khr.rakib@gmail.com>
2020-08-16 19:32:56 +0600

    deploy my react app to github

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const Pagination = ({ showPerPage, onPaginationChange }) => {
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    const value = showPerPage * counter;
+    onPaginationChange(value - showPerPage, value);
+  }, [counter]);
+
+  return (
+    <div className="d-flex justify-content-between mt-4">
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
